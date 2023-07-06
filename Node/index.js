e2dd1d45fd65c2a7d98fb4c61e10acc8bd2f8b0b
giovannebohms <giovannebohms@gmail.com>
2023-07-06 10:08:54 -0300

    chalk 5.0.1

--- Node/index.js
+++ Node/index.js
@@ -1,1 +1,5 @@
+console.log('ão geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento ');
+
+console.log('olá mundo');
+console.log('olá mundo');
 console.log('olá mundo');
