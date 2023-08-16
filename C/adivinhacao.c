987df6acaa6e80d83f3ed70300dfa7a3bd08478b
giovannebohms <giovannebohms@gmail.com>
2023-08-16 15:37:22 -0300

    2 tentativas

--- C/adivinhacao.c
+++ C/adivinhacao.c
@@ -9,10 +9,22 @@ main() {
     printf("\n ********************************");
     printf("\n Bem vindo ao jogo de Adivinhacao");
     printf("\n ********************************");
-  
-    printf("\n Digite um numero   ");
-    scanf("%d", &chute);
-    printf("\n o numero digitado foi %d e o numero secreto eh %d", chute, numerosecreto);
 
+    for (int i = 1; i<3;i++){
 
+        printf("\n Digite um numero   ");
+        scanf("%d", &chute);
+        int acertou = chute==numerosecreto;
+
+        if(acertou){
+            printf("Parabens! Voce acertou!");
+            break;
+        }else{
+            if(chute > numerosecreto){
+                printf("o numero secreto eh menor \n");
+            }else{
+                printf("o numero secreto eh maior");
+            } 
+        }
+    }
 }
