a16a528b4f8b491c6a7d9a25a15a2c27c63d585a
giovannebohms <giovannebohms@gmail.com>
2023-08-16 16:09:41 -0300

    DEFINE

--- C/adivinhacao.c
+++ C/adivinhacao.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-main() {
+#define NUMERO_DE_TENTATIVAS 5
 
+main() {
     int numerosecreto = 42,chute;
-   
+    
     printf("\n ********************************");
     printf("\n Bem vindo ao jogo de Adivinhacao");
     printf("\n ********************************");
 
-    for (int i = 1; i<=3;i++){
+    for (int i = 1; i<=NUMERO_DE_TENTATIVAS;i++){
 
-        printf("\ntentativa %d de 3",i);
+        printf("\ntentativa %d de %d",i,NUMERO_DE_TENTATIVAS);
         printf("\n Digite um numero   ");
         scanf("%d", &chute);
         int acertou = chute==numerosecreto;
