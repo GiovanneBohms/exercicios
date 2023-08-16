e5184bf717338449ad911dc62807bee9cd6414ec
giovannebohms <giovannebohms@gmail.com>
2023-08-16 16:59:32 -0300

    exerciocio: imprime de 1 a 100

--- C/exercicioAlura.c
+++ C/exercicioAlura.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 
 main(){
-    int chute = 0;
-    int numerosecreto = 42;
-    int acertou = chute == numerosecreto;
-if(acertou) {
-    printf("Parabéns! Você acertou!\n");
-} else {
-    if(chute > numerosecreto) {
-        printf("Seu chute foi maior do que o número secreto!\n");
-    } 
-    if(chute < numerosecreto) {
-        printf("Seu chute foi menor do que o número secreto!\n");
+    for(int i = 1; i<=100;i++){
+        printf("%d \n",i);
     }
-}
 }
