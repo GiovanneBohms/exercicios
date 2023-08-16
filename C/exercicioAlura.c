fcd496dfea8323f906e6086b49da4ddd6724ad2a
giovannebohms <giovannebohms@gmail.com>
2023-08-16 17:08:36 -0300

    soma total de 1 a 100

--- C/exercicioAlura.c
+++ C/exercicioAlura.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 main(){
-    int i=0;
 
+    int i=0;
+    int SOMA_TOTAL=0;
     while(1){
-    i++;
-    printf("%d\n",i);
+        i++;
+        SOMA_TOTAL = i+SOMA_TOTAL;
 
         if(i==100){
+            printf("%d", SOMA_TOTAL);
             break;
         }
     }
