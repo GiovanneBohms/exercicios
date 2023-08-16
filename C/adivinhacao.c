5311204fc8387fa277c238f1efb6730fddb7e97c
giovannebohms <giovannebohms@gmail.com>
2023-08-16 14:25:05 -0300

    variaveis

---
+++ C/adivinhacao.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+main() {
+
+    int numerosecreto = 42,chute;
+   
+
+    printf("\n ********************************");
+    printf("\n Bem vindo ao jogo de Adivinhacao");
+    printf("\n ********************************");
+  
+    printf("\n Digite um numero   ");
+    scanf("%d", &chute);
+    printf("\n o numero digitado foi %d e o numero secreto eh %d", chute, numerosecreto);
+
+
+}
