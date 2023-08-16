f52e92af25a7c30591c6f5b43c62264974df8096
giovannebohms <giovannebohms@gmail.com>
2023-08-16 14:25:42 -0300

    multiplicacao

---
+++ C/exercicioAlura.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main(){
+    int x,y,multiplicacao;
+
+    printf("digite dois numeros \n");
+    scanf("%d %d",&x, &y);
+    multiplicacao = x*y;
+    printf("a multiplicacao deu %d", multiplicacao);
+}
