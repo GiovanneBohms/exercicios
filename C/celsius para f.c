e3e5fa9230c36931a897482c7c293afb7541138f
giovannebohms <giovannebohms@gmail.com>
2023-08-27 14:58:51 -0300

    celcius para F

---
+++ C/celsius para f.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main(){
+    int c;
+    float fi;
+    printf("digite a temperatura em Celsius\n");
+    scanf("%d",&c);
+   
+   fi=((9.0/5.0)*c)+32;
+
+   printf("%.0f",fi);
+
+}
