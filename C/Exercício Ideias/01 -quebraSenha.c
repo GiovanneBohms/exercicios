e6482f8a52ceb837893b24ed72fe9e90bb754669
giovannebohms <giovannebohms@gmail.com>
2023-09-05 16:44:44 -0300

    quebra senha

---
+++ C/Exercício Ideias/01 -quebraSenha.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char caracteres[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+    char combinacao[5];
+    combinacao[5] ='\0';
+    int num_caracteres = 36; // Número de caracteres possíveis (dígitos + letras minúsculas)
+    
+
+    for (int i = 0; i < num_caracteres; i++) {
+        for (int j = 0; j < num_caracteres; j++) {
+            for (int k = 0; k < num_caracteres; k++) {
+                for (int l = 0; l < num_caracteres; l++) {
+                    // printf("%c%c%c%c\n", caracteres[i], caracteres[j], caracteres[k], caracteres[l]);
+                    combinacao[0]=caracteres[i];
+                    combinacao[1]=caracteres[j];
+                    combinacao[2]=caracteres[k];
+                    combinacao[3]=caracteres[l];
+
+                    printf("%s \n",combinacao);
+                }
+            }
+        }
+    }
+
+
+
+    return 0;
+}
