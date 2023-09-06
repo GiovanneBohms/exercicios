3eb50191d2a2a9e503db4c0d4870bfccdee1282e
giovannebohms <giovannebohms@gmail.com>
2023-09-05 22:56:26 -0300

    digitar senha

--- C/Exercício Ideias/01 -quebraSenha.c
+++ C/Exercício Ideias/01 -quebraSenha.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
 int main() {
     char caracteres[] = "0123456789abcdefghijklmnopqrstuvwxyz";
-    char combinacao[5];
+    char combinacao[5],senha[5];
     combinacao[4] = '\0';
-    char senha[] = "00aa";
-    int num_caracteres = 36; // Número de caracteres possíveis (dígitos + letras minúsculas)
-    bool senha_encontrada = false;
+    int num_caracteres = strlen(caracteres),senha_encontrada = 0;
+    int contador = 0;
 
-    for (int i = 0; i < num_caracteres && !senha_encontrada; i++) {
-        for (int j = 0; j < num_caracteres && !senha_encontrada; j++) {
-            for (int k = 0; k < num_caracteres && !senha_encontrada; k++) {
-                for (int l = 0; l < num_caracteres && !senha_encontrada; l++) {
+    printf("Digite uma senha com numeros e letras de 4 digitos:\n");
+    scanf("%s",senha);
+    
+    for (int i = 0; i < num_caracteres && senha_encontrada == 0; i++) {
+        for (int j = 0; j < num_caracteres && senha_encontrada == 0; j++) {
+            for (int k = 0; k < num_caracteres && senha_encontrada == 0; k++) {
+                for (int l = 0; l < num_caracteres && senha_encontrada == 0; l++) {
                     combinacao[0] = caracteres[i];
                     combinacao[1] = caracteres[j];
                     combinacao[2] = caracteres[k];
                     combinacao[3] = caracteres[l];
-
                     printf("%s \n", combinacao);
-
+                    contador++;
                     if (strcmp(senha, combinacao) == 0) {
-                        printf("Senha encontrada\n");
-                        senha_encontrada = true; // Define a senha_encontrada como verdadeira para sair dos loops
+                        printf("Senha encontrada na tentativa %d\nA senha eh %s",contador,combinacao);
+                        senha_encontrada = 1;
+                        break;
                     }
                 }
             }
