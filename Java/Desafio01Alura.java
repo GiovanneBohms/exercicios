02bb79708b1499a289e0a5f73a3431d16c4908a0
giovannebohms <giovannebohms@gmail.com>
2023-09-18 16:45:46 -0300

    ignore

--- Java/Desafio01Alura.java
+++ Java/Desafio01Alura.java
@@ -3,7 +3,6 @@ public class Desafio01Alura {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
 
-        
         double saldo = 2500;
         String nome = "Giovanne Bohms";
         String dadosIniciais = String.format(
@@ -13,10 +12,10 @@ public class Desafio01Alura {
                 "Nome: %s\r\n" +
                 "Tipo conta: Corrente\r\n" +
                 "Saldo inicial: R$ %.2f\r\n" +
-                "***********************\r\n",nome,saldo)
+                "***********************\r\n\n",nome,saldo)
                 ;
         String operacoes =
-                "\nOperações:\n" +
+                "Operações:\n" +
                 "1- Consultar saldos\n" +
                 "2- Receber valor\n" +
                 "3- Transferir valor\n" +
@@ -33,29 +32,29 @@ public class Desafio01Alura {
 
             switch(opcao){
                 case 1:
-                System.out.println("Seu saldo é "+saldo);
+                    System.out.println("Seu saldo é "+saldo);
                 break;
                 case 2:
-                System.out.println("digite o valor que irá receber:");
-                double receber = leitura.nextDouble();
-                saldo +=receber;
-                System.out.println("Saldo atual " + saldo);
+                    System.out.println("digite o valor que irá receber:");
+                    double receber = leitura.nextDouble();
+                    saldo +=receber;
+                    System.out.println("Saldo atual " + saldo);
                 break;
                 case 3:
-                System.out.println("Digite valor que deseja transferir");
-                double transferir = leitura.nextDouble();
-                    if (transferir<saldo) {
-                    saldo-=transferir;
-                    System.out.println("Saldo atual:"+ saldo);
-                    }else {
-                        System.out.println("Tentativa de transferência é maior do que saldo na conta");
-                    }
+                    System.out.println("Digite valor que deseja transferir");
+                    double transferir = leitura.nextDouble();
+                        if ( transferir < saldo ) {
+                        saldo -= transferir;
+                        System.out.println("Saldo atual:"+ saldo);
+                        }else {
+                            System.out.println("Tentativa de transferência é maior do que saldo na conta");
+                        }
                 break;
                 case 4:
                     System.out.println("Saindo do sistema...");
                 break;
                 default:
-                System.out.println("opção inválida");
+                    System.out.println("opção inválida");
                 break;
             }
         }
