4d4cb77b4f1dcdd0993399464fbfecf1bd3dbbf9
giovannebohms <giovannebohms@gmail.com>
2023-11-01 15:23:30 -0300

    02

---
+++ C/Exercícios Unicarioca/algoritmo/01.02.js
@@ -0,0 +1,13 @@
+function alg() {
+  let indice = 13;
+  let soma = 0;
+  let k = 0;
+
+  while (k < indice) {
+    k=k+1;
+    soma = soma+k;
+    console.log(k);
+  }
+  console.log(soma)
+}
+alg();
