89b5dcfd2ec8a262219b7f6911c95bf13e4b3c35
giovannebohms <giovannebohms@gmail.com>
2023-11-04 16:44:18 -0300

    organizado

--- HTML + js/Extensao Lab/popup/content-script.js
+++
@@ -1,7 +0,0 @@
-const botaum = document.getElementById("injectScript");
-
-function olamundo() {
-  console.log("ola");
-}
-
-botaum.addEventListener("click", olamundo);
--- HTML + js/Extensao Lab/popup/popup.js
+++ HTML + js/Extensao Lab/popup/popup.js
@@ -1,1 +1,26 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  // Adicione um ouvinte de evento para o botão
+  var injetarBotao = document.getElementById("botao");
+  injetarBotao.addEventListener("click", function () {
+    // Execute a injeção do script
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      // Obtenha a guia ativa
+      var tab = tabs[0];
+      // Injete o script na guia ativa
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: function () {
+          // Coloque o código que você deseja injetar na página aqui
+          // Por exemplo, você pode usar fetch para carregar e injetar o conteúdo do arquivo injecao/index.js
+          fetch(chrome.extension.getURL("injecao/index.js"))
+            .then((response) => response.text())
+            .then((script) => {
+              var scriptElement = document.createElement("script");
+              scriptElement.textContent = script;
+              document.head.appendChild(scriptElement);
+          });
+        },
+      });
+    });
+  });
+});
