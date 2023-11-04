ba1051c42c22fdf981d6ee1a1a1ef19243511f12
giovannebohms <giovannebohms@gmail.com>
2023-11-04 15:26:56 -0300

    fodase

--- HTML + js/Extensao Lab/my-extension/background.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: function () {
-      // Não é necessário adicionar código aqui
-    },
-  });
-});
