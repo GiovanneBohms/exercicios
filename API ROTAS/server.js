4dfeafcaa98fc315e70a5de2c285914a21d6928b
Giovanne <giovannebohms@gmail.com>
2024-05-20 16:00:46 -0300

    post

--- API ROTAS/server.js
+++ API ROTAS/server.js
@@ -1,21 +1,10 @@
-import http from 'http';
-
+//import http from 'http';
+import app from "./src/app.js"
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de de express.js",
-    "/livros":"entre na rota livros",
-    "/autores": "entrei na rota autores"
-};
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(rotas[req.url]);
-    console.log(req) 
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("servidor escutando na porta " + PORT);
     
 });
