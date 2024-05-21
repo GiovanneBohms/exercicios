577e49fae0dddecf916d3593a2ded52185dbf494
Giovanne <giovannebohms@gmail.com>
2024-05-21 18:35:04 -0300

    OBRIGADO DEUS

--- APIROTAS/src/config/dbConnect.js
+++ APIROTAS/src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 async function conectaNaDatabase() {
-  mongoose.connect("mongodb+srv://LivrariaBohms:admin123@cluster0.sihpat0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+  mongoose.connect("mongodb+srv://LivrariaBohms:admin123@cluster0.sihpat0.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
   return mongoose.connection;
 };
 
